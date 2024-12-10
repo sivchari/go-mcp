d11094ae404fa5b8181f282825cd6c2db3b8e813
sivchari <shibuuuu5@gmail.com>
2024-12-10 20:18:20 +0900

    implement prompt list
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- server/prompt.go
+++ server/prompt.go
@@ -14,12 +14,22 @@ import (
 func (s *Server) Prompts(ctx context.Context, msg json.RawMessage) json.Unmarshaler {
 	var req apis.ListPromptsRequest
 	if err := json.Unmarshal(msg, &req); err != nil {
-		s.logger.Error("failed to unmarshal request", slog.String("err", err.Error()))
+		s.logger.Error("failed to unmarshal request",
+			slog.String("method", internal.MethodListPrompts),
+			slog.String("err", err.Error()),
+		)
 		return Error(ctx, internal.CodeInvalidParams, err)
 	}
 	nextCursor := s.nextCursor(req.Params.Cursor)
-	prompts := s.cursorPrompts(nextCursor)
+	prompts := s.cursorPrompts(req.Params.Cursor)
 	if prompts == nil {
+		if req.Params.Cursor != nil {
+			s.logger.Error("invalid cursor",
+				slog.String("cursor", *req.Params.Cursor),
+				slog.String("method", internal.MethodListPrompts),
+			)
+			return Error(ctx, internal.CodeInvalidParams, nil)
+		}
 		return &apis.ListPromptsResult{}
 	}
 	return &apis.ListPromptsResult{
@@ -31,25 +41,25 @@ func (s *Server) Prompts(ctx context.Context, msg json.RawMessage) json.Unmarsha
 func (s *Server) nextCursor(cursor *string) *string {
 	var nextCursor *string
 	if cursor != nil && *cursor != "" {
-		for i, token := range slices.Collect(maps.Keys(s.prompts)) {
+		for i, token := range slices.Sorted(maps.Keys(s.prompts)) {
 			if token == *cursor {
 				if i+1 < len(s.prompts) {
-					nextCursor = &slices.Collect(maps.Keys(s.prompts))[i+1]
+					nextCursor = &slices.Sorted(maps.Keys(s.prompts))[i+1]
 				}
 			}
 		}
 	}
-	if nextCursor == nil && len(s.prompts) > 0 {
-		nextCursor = &slices.Collect(maps.Keys(s.prompts))[0]
+	if (cursor == nil || *cursor == "") && len(s.prompts) > 1 {
+		nextCursor = &slices.Sorted((maps.Keys(s.prompts)))[1]
 	}
 	return nextCursor
 }
 
 func (s *Server) cursorPrompts(cursor *string) []apis.Prompt {
-	if cursor == nil && len(s.prompts) == 0 {
+	if len(s.prompts) == 0 {
 		return nil
 	}
-	if cursor == nil {
+	if cursor == nil || *cursor == "" {
 		return slices.Collect(maps.Values(s.prompts))[0]
 	}
 	return s.prompts[*cursor]
